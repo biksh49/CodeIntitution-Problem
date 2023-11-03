14e877dd4fcbdb7d44324cd279e035f2ced18eca
lenovo <lenovo@L-23-09>
2023-11-03 13:28:53 +0545

    Add project files.

---
+++ CodeIntitution/Array/RowTraversal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeIntitution.Array
+{
+    internal class RowTraversal
+    {
+        
+
+        public int[] rowMajorTraversal(int[,] matrix)
+        {
+            
+            // always check the parameter is null or not in any problem
+            if (matrix.Length == 0)
+            {
+                return new int[0];
+            }
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int totalElements= rows * cols;
+
+            int[] path = new int[totalElements];
+
+            int index = 0;
+            
+            for(int i=0; i<rows; i++)
+            {
+                for(int j=0; j<cols; j++)
+                {
+                    path[index++]= matrix[i,j];
+                }
+            }
+            return path;
+
+        }
+    }
+}
---
+++ CodeIntitution/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodeIntitution.Array;
+
+Console.WriteLine("Hello, World!");
+
+#region Array
+
+#region Row-Major Traversal Problem
+RowTraversal rowTraversal = new RowTraversal();
+//Problem:
+
+//Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+//Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//Explanation: Above is the row-major traversal of the given matrix
+
+int[,] matrix = new int[,]
+{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+rowTraversal.rowMajorTraversal(matrix);
+#endregion 
+
+#endregion
